0ca5380778dc6633935e0ae23299b6a3e651f634
Den <114867235+rosh1ajin@users.noreply.github.com>
2022-10-15 23:39:29 +0300

    Add files via upload

---
+++ Ned_1-2/1/main.cpp
@@ -0,0 +1,13 @@
+// Вывести на экран Первую букву имени из символов "*" (звездочка)
+#include <iostream>
+int main(){
+std::cout << "  ******  " << std::endl
+          << "  *    *  " << std::endl
+          << "  *    *  " << std::endl
+          << "  *    *  " << std::endl
+          << " ******** " << std::endl
+          << " *      * " << std::endl;
+std::getchar();
+std::getchar();
+return 0;
+}
