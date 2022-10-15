9db34368efe5dd9fae236542fec2419d4ae8a843
Den <114867235+rosh1ajin@users.noreply.github.com>
2022-10-15 23:44:21 +0300

    Add files via upload

---
+++ Ned_1-2/2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+#define PI 3.14159265
+
+int main() {
+   double a, b, alpha, h=0;
+   cin >> a >> b >> alpha;
+   h = ((a-b) / 2 ) * tan(alpha);
+   cout << h * ( (a+b) / 2);
+   return 0;
+}
