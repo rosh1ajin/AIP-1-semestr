860c8f5cb0ab7ff261b6d8440a3628c3c2b4a71a
Den <114867235+rosh1ajin@users.noreply.github.com>
2022-10-16 15:14:42 +0300

    Add files via upload

---
+++ 3-4/1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+    {
+        std::cout << "Computer monitor 144Hz";
+            std::cout << "    Philips    ";
+               std::cout << "    17000RUB    ";
+                    std::cout << "   First Class   ";
+                       std::cout << "   10 Years   ";
+}
