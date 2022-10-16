52adf2034358c18b5501dcb8ccd280571eb85b1f
Den <114867235+rosh1ajin@users.noreply.github.com>
2022-10-16 13:54:51 +0300

    Add files via upload

---
+++ Ned_1-2/8/main.cpp
@@ -0,0 +1,18 @@
+// |𝑥| + 𝑥ହ. Если x=−2
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main (){
+    int a,b,b1;
+        std :: cin >> a;
+        std :: cin >> b;
+    if (a<-1) {
+        b1 = (abs(a)+(a*5));
+        std :: cout <<"b1= " << b1;
+    }else{
+        std :: cout <<"error";
+    }
+}
+
+
