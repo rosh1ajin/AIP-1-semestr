aebe835695dab0b15f36af8eb4d102574feb7456
Den <114867235+rosh1ajin@users.noreply.github.com>
2022-12-09 22:55:00 +0300

    Add files via upload

---
+++ Strukturi 11-12?/d/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+struct complex {
+    int mat,imag;
+};
+
+void addition(complex a, complex b) {
+    float sumMat, sumImag;
+    sumMat = a.mat + b.mat;
+    sumImag = a.imag + b.imag;
+}
+
+int main() {
+    return 0;
+}
