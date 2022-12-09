ad386364fb3716d2a4827e20788ecfebd7961060
Den <114867235+rosh1ajin@users.noreply.github.com>
2022-12-09 22:52:18 +0300

    Add files via upload

---
+++ Strukturi 11-12?/11/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+struct robot_car {
+    float speed, time, num;
+};
+
+void rcarS(robot_car a) {
+    float S;
+    S = a.speed * a.time;
+}
+
+int main() {
+    return 0;
+}
