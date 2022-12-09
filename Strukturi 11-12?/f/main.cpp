44194b6b440249d3dfa6b73edaf3d96c828fd81e
Den <114867235+rosh1ajin@users.noreply.github.com>
2022-12-09 22:55:56 +0300

    Add files via upload

---
+++ Strukturi 11-12?/f/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+struct Time
+{
+    int hours, minutes, seconds;
+};
+
+void counting(Time s1, Time s2) {
+    float dif;
+    dif = s1.seconds - s2.seconds;
+}
+
+int main() {
+    return 0;
+}
